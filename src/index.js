6cc8b613d06e940b08ce9619aecab163b17291fc
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-02-16 20:59:41 +0100

    App rnning

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import ReactDOM from "react-dom";
 import useFetch from "./useFetch";
 
-function Home(props) {
-	const data = useFetch(`${props.url}`)	;
-	// const [data] = useFetch("https://www.nvaccess.org/addonStore/en/all/2021.2.0.json");
-	// const data = useFetch ("https://jsonplaceholder.typicode.com/todos/1");
+const Home = () => {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
 
-	return (
-		<>
-			{data &&
-			data.map((item, index) => {
-				return <p key={index}>{item.displayName}</p>;
-			})}
-		</>
-	);
+  return (
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
 };
 
-ReactDOM.render(<Home url="https://www.nvaccess.org/addonStore/en/all/2021.2.0.json" />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
