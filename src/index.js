020196261d73ee4751b961cf63eea8e0f90c7e72
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-02-19 11:49:53 +0100

    make heading 1 a live region to notify about release channel

--- src/index.js
+++ src/index.js
@@ -7,18 +7,28 @@ const Home = () => {
   // const [url, setUrl] = useState(`https://jsonplaceholder.typicode.com/${subfix}`);
   const [url, setUrl] = useState("https://raw.githubusercontent.com/nvdaes/nvdastore/master/src/all2021.2.0.json");
   const [channel, setChannel] = useState("all");
+  const [headerSubfix, setHeaderSubfix]= useState("all");
   const handleChannelChange = (event) => {
     setChannel(event.target.value);
   }
   const handleSubmit = (event) => {
     event.preventDefault();
     setUrl(`https://raw.githubusercontent.com/nvdaes/nvdastore/master/src/${channel}2021.2.0.json`);
-  }  
+  if (channel === "stable") {
+	  setHeaderSubfix("stable");
+    } else {
+		if (channel === "beta") {
+			setHeaderSubfix("beta");
+			} else {
+				setHeaderSubfix("all");
+			}
+	};
+  }
 
   const [data] = useFetch([url]);
   return (
     <>
-      <form role="form" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>Select release channel
           <select
           value={channel}
@@ -30,7 +40,7 @@ const Home = () => {
         </label>
           <input type="submit" />
       </form>
-        <h1>Available add-ons</h1>
+        <h1 aria-live="polite" aria-atomic="true">Available add-ons: {headerSubfix}</h1>
         {data &&
         data.map((item, index) => {
           return <h2 key={index}>{item.displayName} {item.versionName}</h2>;
