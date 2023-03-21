ce1f5dcb1e11c421958b88cdd58b140f5da583fc
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-03-21 08:05:15 +0100

    Add Mastodon profile

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,7 @@ const Footer = () => {
 		<>
 		<footer>
 		<p><a href="https://github.com/nvdaes/nvdastore" target="_blank" rel="noopener noreferrer">Source code</a></p>
+		<p><a rel="me" href="https://mastodon.chrisduffley.com/@nvdastore">Mastodon</a></p>
 		</footer>
 		</>
 	)
